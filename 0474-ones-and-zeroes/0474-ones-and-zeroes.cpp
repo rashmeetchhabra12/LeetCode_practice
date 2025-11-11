50d240880ef0cb06f09b9f5b43954538ce4a0803
Rashmeet Chhabra <128827144+rashmeetchhabra12@users.noreply.github.com>
2025-11-11 21:29:58 +0530

    Time: 290 ms (10.29%), Space: 106.6 MB (27.21%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
@@ -1,32 +1,62 @@
 class Solution {
 public:
-    int recur(vector<string>& strs, int m, int n,int zeroes,int ones,int idx,vector<vector<vector<int>>>&dp){
+    int recur(vector<string>& strs, int m, int n, int zeroes, int ones, int idx,
+              vector<vector<vector<int>>>& dp) {
 
-        int sze=strs.size();
-        if(idx==sze) return dp[idx][zeroes][ones]=0;
+        int sze = strs.size();
+        if (idx == sze)
+            return dp[idx][zeroes][ones] = 0;
 
-        if(dp[idx][zeroes][ones]!=-1) return dp[idx][zeroes][ones];
+        if (dp[idx][zeroes][ones] != -1)
+            return dp[idx][zeroes][ones];
 
-        int pick=0;
-        int dont_pick=recur(strs,m,n,zeroes,ones,idx+1,dp);
+        int pick = 0;
+        int dont_pick = recur(strs, m, n, zeroes, ones, idx + 1, dp);
 
-        int curr_ones=0; int curr_zeroes=0;
-        for(auto ch:strs[idx]){
-            if(ch=='0') curr_zeroes++;
-            else curr_ones++;
+        int curr_ones = 0;
+        int curr_zeroes = 0;
+        for (auto ch : strs[idx]) {
+            if (ch == '0')
+                curr_zeroes++;
+            else
+                curr_ones++;
         }
-        if(zeroes+curr_zeroes<=m && ones+curr_ones<=n){
-            pick=recur(strs,m,n,zeroes+curr_zeroes,ones+curr_ones,idx+1,dp)+1;
+        if (zeroes + curr_zeroes <= m && ones + curr_ones <= n) {
+            pick = recur(strs, m, n, zeroes + curr_zeroes, ones + curr_ones,
+                         idx + 1, dp) +
+                   1;
         }
-        return dp[idx][zeroes][ones]= max(pick,dont_pick);
-
+        return dp[idx][zeroes][ones] = max(pick, dont_pick);
     }
 
     int findMaxForm(vector<string>& strs, int m, int n) {
-        int sze=strs.size();
-        
-        vector<vector<vector<int>>>dp(sze+1,vector<vector<int>>(m+1,vector<int>(n+1,-1)));
-        return recur(strs,m,n,0,0,0,dp);
-    
+        int sze = strs.size();
+
+        vector<vector<vector<int>>> dp(
+            sze + 1, vector<vector<int>>(m + 1, vector<int>(n + 1, 0)));
+
+        for (int idx = sze - 1; idx >= 0; idx--) {
+            for (int zeroes = 0; zeroes <=m; zeroes++) {
+                for (int ones = 0; ones <=n; ones++) {
+
+                    int pick = 0;
+                    int dont_pick =dp[idx+1][zeroes][ones];
+
+                    int curr_ones = 0;
+                    int curr_zeroes = 0;
+                    for (auto ch : strs[idx]) {
+                        if (ch == '0')
+                            curr_zeroes++;
+                        else
+                            curr_ones++;
+                    }
+                    if (zeroes + curr_zeroes <= m && ones + curr_ones <= n) {
+                        pick = dp[idx+1][zeroes + curr_zeroes][ones + curr_ones] + 1;
+                    }
+                    dp[idx][zeroes][ones] = max(pick, dont_pick);
+                }
+            }
+        }
+        return dp[0][0][0];
     }
 };
